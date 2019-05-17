604b1bd205d5dc0e09050300c687b92d6c2c722b
unknown <gvdambros@gmail.com>
2019-05-17 07:32:42 -0300

    read opeartions part 2

--- read.js
+++ read.js
@@ -25,3 +25,18 @@ db.movies.find({genres: "Drama", "rating.average": {$gt: 9}}).pretty()
 db.movies.find({genres: "Drama", genres: "Horror"}).pretty()
 
 db.movies.find({$and: [{genres: "Drama"}, {genres: "Horror"}]}).pretty()
+
+// if rating.average == null it will be here
+db.movies.find({"rating.average": {$exists: true, $gt: 9.3}}).pretty()
+
+// if rating.average == null it will not be here
+db.movies.find({"rating.average": {$exists: true, $ne: null, $gt: 9.3}}).pretty()
+
+db.movies.find({"rating.average": {$exists: true, $type: "number", $gt: 9.3}}).pretty()
+
+db.movies.find({summary: { $regex: /government/}}).pretty()
+
+db.movies.find({$expr: { $gt: ["$runtime", "$id"] }}).pretty()
+
+//If rating is greater than 9 we want the movies with 1 million or more visitors, else the number of visitors has to be grater or equal to the number of expected visitors.
+db.boxOffices.find({$expr: {$lt: [{$cond: {if: {$gt: ["$meta.rating", 9]}, then: 1000000, else: "$expectedVisitors"}}, "$visitors"] }})

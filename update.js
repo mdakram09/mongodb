65b60ea7dc5ee300c46a239968d91cfdc2d7fa7f
unknown <gvdambros@gmail.com>
2019-05-27 10:59:35 -0300

    update operations part. 2

--- update.js
+++ update.js
@@ -13,3 +13,83 @@ db.users.updateOne(
 		}
 	}
 )
+
+// create or update
+db.users.updateOne(
+	{
+		name: "Maria"
+	},
+	{
+		$set: {
+			age: 29,
+			hobbies: [
+				{
+						title: "cooking",
+						frequency: 3
+				}
+			]
+		}
+	},
+	{
+		upsert: true
+	}
+)
+
+// delete field
+db.users.updateOne(
+	{
+		name: "Maria"
+	},
+	{
+		$unset: {
+			hobbies: ""
+		}
+	}
+)
+
+// rename field
+db.users.updateOne(
+	{
+		name: "Maria"
+	},
+	{
+		$rename: {
+			age: "totalAge"
+		}
+	}
+)
+
+db.users.updateMany(
+	{
+		hobbies: 
+		{
+			$elemMatch: 
+			{
+				title: "Sports",
+				frequency: 
+				{
+					$gte: 3
+				}
+			}
+		}
+	},
+	{
+		$set: {
+			"hobbies.$.addicted": true
+		}
+	}
+)
+
+db.users.updateMany(
+	{
+		"age":
+			{
+				$gte: 30
+			}
+	},
+	{
+		$inc: {
+			"hobbies.$[].frequency": -1
+		}
+	}
+)
